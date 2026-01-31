445bf0ead6172e6f19824979109ce1f53ab8d5dd
bindu8247 <mbindum2468@gmail.com>
2026-01-31 13:59:23 +0530

    Create Demo1.java

---
+++ Demo1.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+    System.out.println("child labour is a devastation");
+    System.out.print("save birds");
+    System.out.print("save trees");
+    }
+}
