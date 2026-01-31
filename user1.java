fea7b35d98017120ed12d81fed81653d6b787e3c
bindu8247 <mbindum2468@gmail.com>
2026-01-31 15:27:48 +0530

    Create user1.java

---
+++ user1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class user {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        double a=sc.nextFloat();
+        float b=sc.nextFloat();
+        String str1=sc.next();
+        String str2=sc.nextLine();
+        System.out.println(age);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(str1);
+    }
+}
