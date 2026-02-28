6a204f5eef4462eda176d20472a0b35d992ae543
bindu8247 <mbindum2468@gmail.com>
2026-02-28 15:48:00 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class reverse{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a string:");
+        String str=sc.nextLine();
+        String reversed="";
+        for(int i=str.length()-1;i>=0;i--){
+            reversed =reversed+str.charAt(i);
+        }
+        System.out.println("reversed string:"+reversed);
+    }
+}
