3764ad6ab3dddb574a6132132cc61356085e99c9
bindu8247 <mbindum2468@gmail.com>
2026-03-07 14:53:04 +0530

    Add files via upload

--- palindrome.java
+++ palindrome.java
@@ -1,19 +1,23 @@
 import java.util.*;
-public class palindrome {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
-        int temp=num;
+class Palindrome{
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    void pal(){
+        int temp=n;
         int rev=0;
         while(temp!=0){
             rev=rev*10+temp%10;
             temp=temp/10;
-        }
-        if(num==rev){
-            System.out.println("palindrome");
+        }if(n==rev){
+            System.out.println("yes");
         }else{
-            System.out.println("not a palindrome");
+            System.out.println("no");
         }
+            
+        }
+        public static void main(String args[]){
+                Palindrome p=new Palindrome();
+                        p.pal();
     }
     
 }
